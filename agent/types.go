2a155febc07f8f0ddbdb41335164cad163f9a799
Emir Özbir <asddeneme3@gmail.com>
2025-10-29 22:54:24 +0300

    Feat/daemon agent (#5)
    * feat/daemon-agent: Agent adjusted and cuda_event template integration has been completed
    
    * feat/daemon-agent: Dockerfile added for daemon-agent
    
    * feat/daemon-agent: Adjusted entire image and dependencies
    
    * feat/daemon-agent: Adjusted entire scripts
    
    ---------
    
    Co-authored-by: Emir Özbir <emirozbir@Emir-MacBook-Air.local>

---
+++ agent/types.go
@@ -0,0 +1,11 @@
+package main
+
+const (
+	TEMPLATE_FILE_PATH = "templates/cuda_events.bt.tmpl"
+	BT_FILE_PATH       = "/tmp/cuda_events.bt"
+)
+
+type TemplateProbeLib struct {
+	ProbeLib []string
+	LibPath  string
+}
